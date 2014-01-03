5fba53657ff1d9f0cd6b496d5843c74354f4797c
Marko Heijnen <info@markoheijnen.com>
2014-01-03 23:52:02 +0100

    Update styling of the GD editor

--- editors/gd.php
+++ editors/gd.php
@@ -1,36 +1,38 @@
 <?php
 
 class Improved_Image_Editor_GD extends WP_Image_Editor_GD {
-	function filter_grayscale() {
+
+	public function filter_grayscale() {
 		imagefilter( $this->image, IMG_FILTER_GRAYSCALE );
 	}
 
-	function filter_sepia() {
+	public function filter_sepia() {
 		imagefilter( $this->image, IMG_FILTER_GRAYSCALE );
 		imagefilter( $this->image, IMG_FILTER_COLORIZE, 100, 50, 0 );
 	}
 
-	function filter_contrast() {
+	public function filter_contrast() {
 		imagefilter( $this->image, IMG_FILTER_CONTRAST, -40 );
 	}
 
-	function filter_edge() {
+	public function filter_edge() {
 		imagefilter( $this->image, IMG_FILTER_EDGEDETECT );
 	}
 
-	function filter_emboss() {
+	public function filter_emboss() {
 		imagefilter( $this->image, IMG_FILTER_EMBOSS );
 	}
 
-	function filter_gaussian_blur() {
+	public function filter_gaussian_blur() {
 		imagefilter( $this->image, IMG_FILTER_GAUSSIAN_BLUR );
 	}
 
-	function filter_selective_blur() {
+	public function filter_selective_blur() {
 		imagefilter( $this->image, IMG_FILTER_SELECTIVE_BLUR );
 	}
 
-	function filter_negative() {
+	public function filter_negative() {
 		imagefilter( $this->image, IMG_FILTER_NEGATE );
 	}
+
 }
