a446507d833212ef1b56ef145a240176c47a37e3
Marko Heijnen <info@markoheijnen.com>
2014-09-02 22:46:43 +0200

    Start from scratch again after changed in WordPress.

--- improved-image-editor.php
+++ improved-image-editor.php
@@ -13,16 +13,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 	header('HTTP/1.1 403 Forbidden');
 }
 
-include 'inc/overwrite.php';
-
 class Improved_Image_Editor {
+	const version = '0.1';
 
 	public function __construct() {
 		add_action( 'init', array( $this, 'register_scripts_styles' ) );
 		add_action( 'current_screen', array( $this, 'current_screen' ) );
 		add_action( 'wp_enqueue_media', array( $this, 'load_template' ) );
 
-		add_filter( 'admin_footer_text', array( $this, 'show_editor' ) );
 		add_filter( 'wp_image_editors', array( $this, 'wp_image_editors' ) );
 
 		add_filter( 'wp_image_editor_before_change', array( $this, 'wp_image_editor_before_change' ), 10, 2 );
@@ -31,14 +29,15 @@ class Improved_Image_Editor {
 	public function register_scripts_styles() {
 		$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
 
-		wp_register_script( 'improved_image_editor', plugins_url( '/js/main.js', __FILE__ ), array( 'jquery', 'backbone' ) );
+		wp_register_script( 'improved_image_editor', plugins_url( '/js/main.js', __FILE__ ), array( 'jquery', 'backbone' ), self::version );
 
-		wp_register_style( 'improved_image_editor', plugins_url( '/css/metabox.css', __FILE__ ), array( 'media-views' ) );
+		wp_register_style( 'improved_image_editor', plugins_url( '/css/main.css', __FILE__ ), array( 'media-views' ), self::version );
 	}
 
 	public function current_screen( $screen ) {
-		if( $screen->base != 'post' || $screen->post_type != 'attachment' )
+		if( $screen->base != 'post' || $screen->post_type != 'attachment' ) {
 			return;
+		}
 
 		$this->load_template();
 	}
@@ -52,24 +51,18 @@ class Improved_Image_Editor {
 	}
 
 
-	public function show_editor( $text ) {
-		$text .= ' - ' . _wp_image_editor_choose();
-
-		return $text;
-	}
-
 	public function wp_image_editors( $editors ) {
 		include_once 'editors/gd.php';
 		include_once 'editors/imagick.php';
 		include_once 'editors/gmagick.php';
 
-		$editors2 = array(
-			//'Improved_Image_Editor_Gmagick',
-			//'Improved_Image_Editor_Imagick',
+		$new_editors = array(
+			'Improved_Image_Editor_Gmagick',
+			'Improved_Image_Editor_Imagick',
 			'Improved_Image_Editor_GD'
 		);
 
-		$editors = array_merge( $editors2, $editors );
+		$editors = array_merge( $new_editors, $editors );
 
 		return $editors;
 	}
@@ -77,10 +70,10 @@ class Improved_Image_Editor {
 
 	public function wp_image_editor_before_change( $image, $changes ) {
 		foreach ( $changes as $operation ) {
-			if( $operation->type == 'filter' && isset( $operation->filter ) ) {
+			if ( $operation->type == 'filter' && isset( $operation->filter ) ) {
 				$method = 'filter_' . esc_attr( $operation->filter );
 
-				if( method_exists( $image, $method ) ) {
+				if ( method_exists( $image, $method ) ) {
 					call_user_func( array( $image, $method ) );
 				}
 			}
--- inc/overwrite.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Improved_Image_Editor_Overwrite {
-
-
-}
-
-new Improved_Image_Editor_Overwrite();
