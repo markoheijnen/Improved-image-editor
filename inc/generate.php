e8be36f13b28298f2b141b47bef78ec01706454e
Marko Heijnen <info@markoheijnen.com>
2014-09-13 23:51:47 +0200

    Delete images WordPress doesn't delete.

--- inc/generate.php
+++ inc/generate.php
@@ -3,7 +3,8 @@
 class Improved_Image_Editor_Generate {
 
 	public function __construct() {
-		add_filter( 'image_downsize', array( $this, 'generate_image_size' ), 10, 3 );	
+		add_filter( 'image_downsize', array( $this, 'generate_image_size' ), 10, 3 );
+		add_action( 'delete_attachment', array( __CLASS__, '_delete_attachment' ) );
 	}
 
 	public function generate_image_size( $image_data, $id, $size ) {
@@ -18,5 +19,28 @@ class Improved_Image_Editor_Generate {
 
 		return $image_data;
 	}
+
+	public function _delete_attachment( $post_id ) {
+		$file = get_attached_file( $post_id );
+		$meta = wp_get_attachment_metadata( $post_id );
+
+		$intermediate_sizes = array();
+		foreach ( get_intermediate_image_sizes() as $size ) {
+			if ( $intermediate = image_get_intermediate_size( $post_id, $size ) ) {
+				$intermediate_sizes[] = $intermediate['path'];
+			}
+		}
+
+		if ( isset( $meta['sizes'] ) && is_array( $meta['sizes'] ) ) {
+			foreach ( $meta['sizes'] as $size => $sizeinfo ) {
+				$intermediate_file = str_replace( basename( $file ), $sizeinfo['file'], $file );
+
+				if ( ! in_array( $intermediate_file, $intermediate_sizes ) ) {		
+					$intermediate_file = apply_filters( 'wp_delete_file', $intermediate_file );
+					@ unlink( path_join( $uploadpath['basedir'], $intermediate_file ) );
+				}
+			}
+		}
+	} 
 
 }
